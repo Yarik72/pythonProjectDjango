171730fbb8ed4188d94b9a99870cfbf71563057e
Yaroslav <Lazutinyaroslav2021@gmail.com>
2024-12-06 23:33:40 +0500

    проверка на регистрацию

--- UrbanDjango/task5/views.py
+++ UrbanDjango/task5/views.py
@@ -5,7 +5,10 @@ from .UserRegister import UserForm
 # Create your views here.
 
 def sign_up_by_django(request):
-    users = ['user1', 'user2', 'user3']
+    if 'users' not in request.session:
+        request.session['users'] = ['user1', 'user2', 'user3']
+
+    users = request.session['users']
     info = {}
     if request.method == 'POST':
         form = UserForm(request.POST)
@@ -16,8 +19,11 @@ def sign_up_by_django(request):
             age = form.cleaned_data['age']
 
             if password == repeat_password and age >= 18 and username not in users:
+                users.append(username)
+                request.session['users'] = users
                 info['message'] = f"Приветствуем, {username}"
                 form = UserForm()
+
             elif password != repeat_password:
                 info['error'] = 'Пароли не совпадают'
             elif age < 18:
@@ -32,19 +38,24 @@ def sign_up_by_django(request):
 
 
 def sign_up_by_html(request):
-    users = ['user1', 'user2', 'user3']
+
+    if 'users' not in request.session:
+        request.session['users'] = ['user1', 'user2', 'user3']
+
+    users = request.session['users']
     info = {}
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         repeat_password = request.POST.get('repeat_password')
         age = request.POST.get('age')
-
         info['username'] = username
         info['password'] = password
         info['repeat_password'] = repeat_password
         info['age'] = age
         if password == repeat_password and int(age) >= 18 and username not in users:
+            users.append(username)
+            request.session['users'] = users
             info['message'] = f"Приветствуем, {username}"
             info['username'] = ''
             info['password'] = ''
